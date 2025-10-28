fd4ddaecbc4d83369dccd1f0ff076dfb42830ab6
Artem Titov <temik2b@mail.ru>
2025-10-28 17:37:49 +0500

    add class Customer + doc

--- 3.py
+++ 3.py
@@ -26,3 +26,37 @@ class Product:
         self.price = price
         self.stock = stock
         self.category = category
+
+class Customer:
+    """
+    Класс для представления покупателя в онлайн-магазине.
+    
+    Attributes:
+        customer_id (int): Уникальный идентификатор покупателя
+        name (str): Имя покупателя
+        email (str): Email покупателя
+        order_history (list): История заказов покупателя
+    """
+    
+    def __init__(self, customer_id, name, email):
+        """
+        Инициализирует объект покупателя.
+        
+        Args:
+            customer_id (int): Уникальный идентификатор покупателя
+            name (str): Имя покупателя
+            email (str): Email покупателя
+        """
+        self.customer_id = customer_id
+        self.name = name
+        self.email = email
+        self.order_history = []
+
+    def add_order(self, order):
+        """
+        Добавляет заказ в историю заказов покупателя.
+        
+        Args:
+            order (Order): Объект заказа для добавления
+        """
+        self.order_history.append(order)
