2d31d2730d988924288221b2b9c315a9a893dc19
Artem Titov <temik2b@mail.ru>
2025-10-28 16:30:28 +0500

    add doc for class Employee

--- 1.py
+++ 1.py
@@ -1,2 +1,19 @@
 class Employee:
-    pass
+    """
+    Базовый класс для всех сотрудников компании.
+    
+    Атрибуты:
+        name (str): Имя сотрудника
+        salary (float): Базовая зарплата сотрудника
+    """
+    def __init__(self, name, salary):
+        """
+        Конструктор базового класса Employee.
+        
+        Args:
+            name (str): Имя сотрудника
+            salary (float): Базовая зарплата
+        """       
+        self.name = name
+        self.salary = salary
+        
