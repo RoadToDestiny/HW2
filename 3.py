23c2ee6f652d193ae01b31393bd3a3b569e09acf
Artem Titov <temik2b@mail.ru>
2025-10-28 17:39:47 +0500

    add class Shopping Cart + doc

--- 3.py
+++ 3.py
@@ -60,3 +60,71 @@ class Customer:
             order (Order): Объект заказа для добавления
         """
         self.order_history.append(order)
+
+class ShoppingCart:
+    """
+    Класс для представления корзины покупок.
+    
+    Attributes:
+        items (dict): Словарь товаров в корзине
+    """
+    
+    def __init__(self):
+        """Инициализирует пустую корзину покупок."""
+        self.items = {}
+
+    def add_product(self, product, quantity=1):
+        """
+        Добавляет товар в корзину.
+        
+        Args:
+            product (Product): Объект товара для добавления
+            quantity (int, optional): Количество товара. По умолчанию 1
+        """
+        if product.product_id in self.items:
+            self.items[product.product_id]['quantity'] += quantity
+        else:
+            self.items[product.product_id] = {
+                'product': product,
+                'quantity': quantity
+            }
+
+    def remove_product(self, product_id):
+        """
+        Удаляет товар из корзины.
+        
+        Args:
+            product_id (int): Идентификатор товара для удаления
+        """
+        if product_id in self.items:
+            del self.items[product_id]
+
+    def update_quantity(self, product_id, quantity):
+        """
+        Обновляет количество товара в корзине.
+        
+        Args:
+            product_id (int): Идентификатор товара
+            quantity (int): Новое количество товара
+        """
+        if product_id in self.items:
+            if quantity <= 0:
+                self.remove_product(product_id)
+            else:
+                self.items[product_id]['quantity'] = quantity
+
+    def clear(self):
+        """Очищает корзину от всех товаров."""
+        self.items.clear()
+
+    def get_total_price(self):
+        """
+        Рассчитывает общую стоимость товаров в корзине.
+        
+        Returns:
+            float: Общая стоимость товаров в корзине
+        """
+        total = 0
+        for item in self.items.values():
+            total += item['product'].price * item['quantity']
+        return total
